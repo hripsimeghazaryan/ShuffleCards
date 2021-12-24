7b708d74085aee1e2b9c6b4560721c34555102a9
Hripsime Ghazaryan <94200499@mail.ru>
2021-12-24 15:09:42 +0400

    created 6 cards instead of 4

--- shuffle.js
+++ shuffle.js
@@ -1,4 +1,4 @@
-const allColors = ["#FF0000", "#008000", "#FFFF00", "#0000FF"];
+const allColors = ["#FF0000", "#FB9A00", "#FFFF00", "#008000", "#0000FF", "#8700B1"];
 
 const insertCards = () => {
     const container = document.createElement("div");
@@ -14,13 +14,14 @@ const insertCards = () => {
 
 const shuffleColors = () => {
     const container = document.querySelector(".card-container");
-    const first = container.firstChild;
-    const last = container.lastChild;
-    const lastColor = last.previousElementSibling.style.backgroundColor;
-    last.previousElementSibling.style.backgroundColor = last.style.backgroundColor;
-    last.style.backgroundColor = first.nextElementSibling.style.backgroundColor;
-    first.nextElementSibling.style.backgroundColor = first.style.backgroundColor;
-    first.style.backgroundColor = lastColor;
+    const n = allColors.length - 1;
+    let last = container.lastChild;
+    const lastColor = last.style.backgroundColor;
+    for(let i = n; i > 0; i--) {
+        last.style.backgroundColor = last.previousElementSibling.style.backgroundColor;
+        last = last.previousElementSibling;
+    }
+    last.style.backgroundColor = lastColor;
 }
 
 window.addEventListener("load", insertCards);
